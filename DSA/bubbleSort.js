0531c17781626e651e8417b27ebd863e5aadaa27
Mahi <girimahesh.m@gmail.com>
2024-05-30 14:58:20 +0530

    DSA started

---
+++ DSA/bubbleSort.js
@@ -0,0 +1,16 @@
+let bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+};
+
+let arr = [5, 16, 4, 52, 7, 1, 6, 85];
+console.log(bubbleSort(arr));
+
