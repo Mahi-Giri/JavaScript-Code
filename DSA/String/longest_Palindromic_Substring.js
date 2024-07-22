657f48b2b8d4c2db69a06363ba709e7d88309360
Mahi <girimahesh.m@gmail.com>
2024-07-22 12:46:33 +0530

    added new code longest_Palindromic_Substring

--- DSA/String/longest_Palindromic_Substring.js
+++ DSA/String/longest_Palindromic_Substring.js
@@ -1,6 +1,17 @@
 let longestPalindrome = (s) => {
-    //
+    let j = s.length - 1;
+    let i = 0;
+    for (i = 0; i < j; i++, j--) {
+        if (s[i] !== s[j]) {
+            break;
+        }
+    }
+    if (i != j) {
+        return `Not a Palindrome`;
+    } else {
+        return `Palindrome`;
+    }
 };
 
-let result = longestPalindrome("babad");
+let result = longestPalindrome("madam");
 console.log(result);
